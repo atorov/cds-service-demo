b7b7e4f04e6bdf59b9272532a97fea9502e4c2a6
Veselin <veselin.lk.petrov@gmail.com>
2019-07-21 19:22:42 +0300

    updated info-card and card-age-prefetch (previously card-age) and start implementing card-age-fhir-request services (still in progress)

--- app.js
+++ app.js
@@ -48,19 +48,25 @@ const server = http.createServer((req, res) => {
     return res.end(JSON.stringify({
       services: [
         {
+          id: 'info-card',
           hook: 'patient-view',
           title: 'Info card on patient view',
-          description: 'An example of a CDS Service that returns an info card',
-          id: 'info-card'
+          description: 'An example of a CDS Service that returns an info card'
         },
         {
+          id: 'card-age-prefetch',
           hook: 'patient-view',
           title: 'Card on patient view with patient age',
-          description: 'An example of a CDS Service that returns a card containing the patient age. The card could be of type `info`, `warning` or `critical` and it depends on the patient age.',
-          id: 'card-age',
+          description: 'An example of a CDS Service that returns a card containing the patient age. The card could be of type `info`, `warning` or `critical` and it depends on the patient age. The information about the patient is provided by the CDS Client in `prefetch` property.',
           prefetch: {
             patient: 'Patient/{{context.patientId}}'
           }
+        },
+        {
+          id: 'card-age-fhir-request',
+          hook: 'patient-view',
+          title: 'Card on patient view with patient age',
+          description: 'An example of a CDS Service that returns a card containing the patient age. The card could be of type `info`, `warning` or `critical` and it depends on the patient age. The information about the patient is retrieved by the service itself.'
         }
       ]
     }))
@@ -90,8 +96,8 @@ const server = http.createServer((req, res) => {
     }))
   }
 
-  // OPTIONS, GET /cds-services/card-age ---------------------------------------
-  if (url == '/cds-services/card-age' && ['OPTIONS', 'POST'].includes(method.toUpperCase())) {
+  // OPTIONS, GET /cds-services/card-age-prefetch ------------------------------
+  if (url == '/cds-services/card-age-prefetch' && ['OPTIONS', 'POST'].includes(method.toUpperCase())) {
     return processPost(req, () => {
       res.setHeader('Content-Type', 'application/json')
       res.setHeader('Access-Control-Allow-Credentials', 'true')
@@ -139,6 +145,61 @@ const server = http.createServer((req, res) => {
             url: 'https://example.com/source'
           }
         })
+      }
+
+      return res.end(JSON.stringify({ cards }))
+    })
+  }
+
+  // OPTIONS, GET /cds-services/card-age-fhir-request --------------------------
+  if (url == '/cds-services/card-age-fhir-request' && ['OPTIONS', 'POST'].includes(method.toUpperCase())) {
+    return processPost(req, () => {
+      res.setHeader('Content-Type', 'application/json')
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST')
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      res.writeHead(200)
+
+      const hook = req.post && req.post.hook
+      // const birthDate = req.post && req.post.prefetch && req.post.prefetch.patient && req.post.prefetch.patient.birthDate
+      const cards = []
+      if (hook === 'patient-view') {
+        // const age = birthDate && moment(birthDate).isValid() ? moment().diff(birthDate, 'years') : null
+
+        // let detail = `### Details\n\nInvalid date of birth \n\n`
+        // if (age !== null && age >= 0) {
+        //   detail = `Patient age: ${age} ${age > 1 ? 'years' : 'year'}\n\n`
+        // }
+
+        // let indicator = 'info'
+        // if (age !== null && (age < 10 || age > 80)) {
+        //   indicator = 'warning'
+        // } else if (age === null || age < 0) {
+        //   indicator = 'critical'
+        // }
+
+        // switch (indicator) {
+        //   case 'info':
+        //     detail += `![Image of something](https://picsum.photos/id/450/640/240)\n\n`
+        //     break
+        //   case 'warning':
+        //     detail += `![Image of something](https://picsum.photos/id/476/640/240)\n\n`
+        //     break
+        //   case 'critical':
+        //     detail += `![Image of something](https://picsum.photos/id/180/640/240)\n\n`
+        //     break
+        // }
+
+        // cards.push({
+        //   summary: 'Patient age check',
+        //   indicator,
+        //   detail,
+        //   source: {
+        //     label: 'CDS Service Demo',
+        //     url: 'https://example.com/source'
+        //   }
+        // })
       }
 
       return res.end(JSON.stringify({ cards }))
